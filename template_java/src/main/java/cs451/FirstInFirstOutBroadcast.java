0d633172e3f380592e0e1d22ab6197b29b5b6792
Aleksandar Hrusanov <aleksandar.hrusanov@epfl.ch>
2020-11-08 01:05:15 +0100

    First FIFO implementation. TODO: Issue with URB

---
+++ template_java/src/main/java/cs451/FirstInFirstOutBroadcast.java
@@ -0,0 +1,127 @@
+package cs451;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.SortedSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class FirstInFirstOutBroadcast {
+	private Process process;
+	private UniformReliableBroadcast urb;
+	
+	// contains the number of messages received by respective pid (e.g. highest message id received)
+	private int[] vectorClock;
+	private ConcurrentHashMap<Integer, ConcurrentSkipListSet<Message>> pending;
+	
+	public FirstInFirstOutBroadcast(UniformReliableBroadcast urb) {
+		this.urb = urb;		
+		this.process = urb.getProcess();
+	}
+	
+	public void fifoBroadcast(String content, int msgId) {
+		if (this.vectorClock == null) {
+			int processCount = this.process.getAllProcesses().size();
+			this.vectorClock = new int[processCount];
+		}
+		
+		this.urb.urbBroadcast(content, msgId);
+		this.vectorClock[this.process.getProcessId() - 1]++;
+	}
+	
+	public void fifoDeliver(Message msg, InetSocketAddress source) {
+		int processCount = this.process.getAllProcesses().size();
+		if (this.pending == null) {
+			this.pending = new ConcurrentHashMap<Integer, ConcurrentSkipListSet<Message>>();
+			// initialize pending
+			this.process = urb.getProcess();
+
+			for (int i = 1; i <= processCount; i++) {
+				ConcurrentSkipListSet<Message> skipListSet = new ConcurrentSkipListSet<Message>(
+						(m1, m2) -> {
+							if (m1.getMsgId() < m2.getMsgId()) {
+								return -1;
+							} else if (m1.getMsgId() > m2.getMsgId()) {
+								return 1;
+							}
+							return 0;
+				});
+				this.pending.put(i, skipListSet);
+			}
+		}
+		if (this.vectorClock == null) {
+			this.vectorClock = new int[processCount];
+		}
+		
+		boolean urbDelivered = this.urb.urbDeliver(msg , source);
+		System.out.println("TRYING msg " + msg.getMsgId() + " from " + msg.getOriginalPid());
+		if (!urbDelivered) {
+			System.out.println("URB failed msg " + msg.getMsgId() + " from " + msg.getOriginalPid());
+		}
+		if (urbDelivered) {			
+			int pid = msg.getOriginalPid();
+			ConcurrentSkipListSet<Message> relevantPending = this.pending.get(pid);
+
+			// check vector clock - should we try to deliver this message?
+			if (msg.getMsgId() == (this.vectorClock[pid-1] + 1)) {
+				this.vectorClock[pid-1]++;
+				System.out.println("FIFO deliver msg " + msg.getMsgId() + " from process " + msg.getOriginalPid());
+				// if this is a message we are expecting, go over pending and try to urbDeliver
+				// messages from the same source
+				ConcurrentSkipListSet<Message> tempPending = new ConcurrentSkipListSet<Message>(relevantPending);
+				for (Message m : relevantPending) {
+//					boolean delivered = this.urb.urbDeliver(m , source);
+					if (m.getMsgId() == (this.vectorClock[pid-1]+1)) {
+//					if (delivered) {
+						// if we successfully delivered message, update vector clock
+						this.vectorClock[pid - 1] = this.vectorClock[pid-1] + 1;
+						// and remove message from pending
+						tempPending.remove(m);
+						System.out.println("FIFO deliver msg " + m.getMsgId() + " from process " + msg.getOriginalPid());
+					} else {
+						break;
+					}
+				}
+				this.pending.put(pid, tempPending);
+			} else {
+				relevantPending.add(msg);
+				this.pending.put(pid, relevantPending);
+			}
+		}
+
+		
+		
+		
+//		// put msg in pending
+//		int pid = msg.getOriginalPid();
+//		ConcurrentSkipListSet<Message> relevantPending = this.pending.get(pid);
+//
+//		relevantPending.add(msg);
+//		this.pending.put(pid, relevantPending);
+//		boolean del = this.urb.urbDeliver(msg , source);
+//
+//		// check vector clock - should we try to deliver this message?
+//		if (msg.getMsgId() == (this.vectorClock[pid-1] + 1)) {
+//			System.out.println("Passed VC test :: VC[" + (pid - 1) + "] = " + this.vectorClock[pid-1] + " :: msg ID = " + msg.getMsgId());
+//			// if this is a message we are expecting, go over pending and try to urbDeliver
+//			// messages from the same source
+//			ConcurrentSkipListSet<Message> tempPending = new ConcurrentSkipListSet<Message>(relevantPending);
+//			for (Message m : relevantPending) {
+//				boolean delivered = this.urb.urbDeliver(m , source);
+//
+//				if (delivered) {
+//					// if we successfully delivered message, update vector clock
+//					this.vectorClock[pid - 1] = this.vectorClock[pid-1] + 1;
+//					// and remove message from pending
+//					tempPending.remove(m);
+//				} else {
+//					// if cannot urb deliver, do not iterate through pending anymore
+//					break;
+//				}
+//			}
+//			this.pending.put(pid, tempPending);
+//		}
+		
+			
+	}
+}
